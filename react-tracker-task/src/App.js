741f67da168cdca5cae07de6e1f7e90023e26eb7
ishika <imusun24@gmail.com>
2021-04-08 18:01:19 +0530

    app

--- react-tracker-task/src/App.js
+++ react-tracker-task/src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
+    <div className='conatiner'>
+      <header/>
     </div>
-  );
+  )
 }
 
 export default App;
